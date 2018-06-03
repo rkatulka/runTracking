fd54e44eb453fb922b833cf0c3691895e8ba18d8
Robert Katulka <bobby.katulka@gmail.com>
2018-06-03 10:26:58 -0400

    Adding a new toolbar to the site.  The toolbar is very basic and links only work for google.

--- runTracking/src/app/app.module.ts
+++ runTracking/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { MaterialModule } from '../material/material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -10,7 +13,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
